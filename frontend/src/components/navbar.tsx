d5c4f48fa0800d29ade91581cfbc01145751d0d4
Mitchshona <136311675+Mitchshona@users.noreply.github.com>
2025-03-31 17:11:35 +0800

    bug

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { ShoppingBasketIcon as Basketball, Menu } from "lucide-react"
+import { ShoppingBasketIcon as Menu } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
